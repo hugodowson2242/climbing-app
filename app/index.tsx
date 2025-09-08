6586de7bac50e3d1c5b3302dd6b91d2d9f379792
hugodowson2242 <hugo.dowson@intercom.io>
2025-09-08 19:42:37 +0100

    header

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import PlannedExerciseList from "@/components/planned-exercise-list/planned-exercise-list";
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 import { ScrollView, Text, View } from "react-native";
 
 export default function Index() {
@@ -9,17 +9,24 @@ export default function Index() {
   const athleteId = athlete ? parseInt(athlete as string) : undefined;
 
   return (
-    <ScrollView style={{ flex: 1 }}>
-      <View style={{ flex: 1, padding: 8 }}>
-        {sessionId && athleteId ? (
-          <PlannedExerciseList
-            sessionId={sessionId}
-            athleteId={athleteId}
-          />
-        ) : (
-          <Text>No session parameters provided</Text>
-        )}
-      </View>
-    </ScrollView>
+    <>
+      <Stack.Screen
+        options={{
+          title: "Training Session"
+        }}
+      />
+      <ScrollView style={{ flex: 1 }}>
+        <View style={{ flex: 1, padding: 8 }}>
+          {sessionId && athleteId ? (
+            <PlannedExerciseList
+              sessionId={sessionId}
+              athleteId={athleteId}
+            />
+          ) : (
+            <Text>No session parameters provided</Text>
+          )}
+        </View>
+      </ScrollView>
+    </>
   );
 }
